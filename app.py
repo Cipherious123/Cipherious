8755ee24682e8eca6ed93696a094052c566da51b
Cipherious123 <cipherious.nps@gmail.com>
2024-11-04 03:02:42 +0000

    modified:   app.py

--- app.py
+++ app.py
@@ -130,11 +130,11 @@ def check_combo(ciphername,password):
         allowed = "uan"
         if len(password) != 3:
             errorr.raise_issue("Please refer to help box for how to input the password. There must be 4 parts split by spaces")
-        if not int_check(password[0]):
+        elif not int_check(password[0]):
             errorr.raise_issue("Base to convert your number to must be positive integers from 2 to 95. Please refer to help box or the Base changer page\n")
-        if not 1 < int( password[0] ) < 96:
+        elif not 1 < int( password[0] ) < 96:
             errorr.raise_issue("Base in and Base out must be positive integers from 2 to 95. Please refer to help box or the Base changer page")
-        if password[1] not in allowed or password[2] not in allowed:
+        elif password[1] not in allowed or password[2] not in allowed:
             errorr.raise_issue("Please refer to help box for how to input the password. The number systems must be denoted as given there")
 
     else:       
