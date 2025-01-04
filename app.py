e11db191fbc8b853bfce0f739967edffc3c1b0fc
Cipherious123 <cipherious.nps@gmail.com>
2025-01-04 07:41:38 +0000

    modified:   app.py

--- app.py
+++ app.py
@@ -10,13 +10,13 @@ from keygen import new_int, key_gen
 app = Flask(__name__, template_folder='templates')
 
 app.secret_key = 'your_secret_key'  # Needed to use sessions in Flask
-DATABASE = "postgresql://nishant11:pyi4dOnRekamRKcMnj6pZkQRF2ef6WSo@dpg-ct4i2ibtq21c7393omdg-a.singapore-postgres.render.com/users_tpju"
+DATABASE = "postgresql://nishant:aVIjFtkaE986KvUpDmdG6PK06ro28Yul@dpg-ctseb652ng1s73c0fd5g-a.singapore-postgres.render.com/users_y783"
 def send_cursor():   
     conn = psycopg2.connect( # Connect to your PostgreSQL database
-        dbname="users_tpju",
-        user="nishant11",
-        password="pyi4dOnRekamRKcMnj6pZkQRF2ef6WSo",
-        host="dpg-ct4i2ibtq21c7393omdg-a",
+        dbname="users_y783",
+        user="nishant",
+        password="aVIjFtkaE986KvUpDmdG6PK06ro28Yul",
+        host="dpg-ctseb652ng1s73c0fd5g-a",
         port="5432"
     )
     cursor = conn.cursor()
